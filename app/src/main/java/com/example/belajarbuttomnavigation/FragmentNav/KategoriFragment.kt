62cad6508429417e9f1603ceb36460bb0f55bb04
ArsyalRaka02 <saigusa231@gmail.com>
2019-10-24 07:09:39 +0700

    Basic Buttom  Navigation

---
+++ app/src/main/java/com/example/belajarbuttomnavigation/FragmentNav/KategoriFragment.kt
@@ -0,0 +1,21 @@
+package com.example.belajarbuttomnavigation.FragmentNav
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.belajarbuttomnavigation.R
+
+class KategoriFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater!!.inflate(R.layout.fragment_kategori, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/belajarbuttomnavigation/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.belajarbuttomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.example.belajarbuttomnavigation.FragmentNav.HomeFragment
+import com.example.belajarbuttomnavigation.FragmentNav.KategoriFragment
+import com.example.belajarbuttomnavigation.FragmentNav.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var homeFragment : HomeFragment
+    lateinit var profileFragment : ProfileFragment
+    lateinit var kategoriFragment : KategoriFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_nav)
+
+        bottomNavigation.setOnNavigationItemSelectedListener {item ->
+            when (item.itemId){
+                R.id.home -> {
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.kategori -> {
+                    kategoriFragment = KategoriFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame, kategoriFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.profile -> {
+                    profileFragment = ProfileFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.profile,profileFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
+
+            true
+
+        }
+
+    }
+}
